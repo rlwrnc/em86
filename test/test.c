5bb98718347e7dc68dd2f6e4af89b2ca730447b7
rlwrnc <raymond.lawrence@tutanota.com>
2023-10-25 19:48:15 -0400

    fixed more tests

--- test/test.c
+++ test/test.c
@@ -2,8 +2,15 @@
 #include "../source/em86.h"
 #include <stdint.h>
 
-#define test_decode_instruction(expected, actual) \
-	do {}
+#define TEST_INSTRUCTION(instruction, ex_type, ex_num_bytes, ex_dest, ex_source, ex_disp, ex_data) \
+	do { \
+		TEST_ASSERT_EQUAL_UINT(ex_type, instruction.type); \
+		TEST_ASSERT_EQUAL_UINT(ex_num_bytes, instruction.number_of_bytes); \
+		TEST_ASSERT_EQUAL_UINT16(ex_dest, instruction.destination); \
+		TEST_ASSERT_EQUAL_UINT16(ex_source, instruction.source); \
+		TEST_ASSERT_EQUAL_UINT16(ex_disp, instruction.displacement); \
+		TEST_ASSERT_EQUAL_UINT16(ex_data, instruction.data); \
+	} while (0)
 
 void setUp()
 {
@@ -21,12 +28,7 @@ void test_decode_mov_reg_to_reg_byte()
 	};
 
 	Instruction instruction = decode_instruction(bytes);
-	TEST_ASSERT_EQUAL_UINT(MOV_RM_REG, instruction.type);
-	TEST_ASSERT_EQUAL_UINT(2, instruction.number_of_bytes);
-	TEST_ASSERT_EQUAL_UINT16(CH, instruction.destination);
-	TEST_ASSERT_EQUAL_UINT16(AH, instruction.source);
-	TEST_ASSERT_EQUAL_UINT16(0, instruction.displacement);
-	TEST_ASSERT_EQUAL_UINT16(0, instruction.data);
+	TEST_INSTRUCTION(instruction, MOV_RM_REG, 2, CH, AH, 0, 0);
 }
 
 void test_decode_mov_reg_to_reg_wide()
@@ -37,12 +39,7 @@ void test_decode_mov_reg_to_reg_wide()
 	};
 
 	Instruction instruction = decode_instruction(bytes);
-	TEST_ASSERT_EQUAL_UINT(MOV_RM_REG, instruction.type);
-	TEST_ASSERT_EQUAL_UINT(2, instruction.number_of_bytes);
-	TEST_ASSERT_EQUAL_UINT16(DX, instruction.destination);
-	TEST_ASSERT_EQUAL_UINT16(BX, instruction.source);
-	TEST_ASSERT_EQUAL_UINT16(0, instruction.displacement);
-	TEST_ASSERT_EQUAL_UINT16(0, instruction.data);
+	TEST_INSTRUCTION(instruction, MOV_RM_REG, 2, DX, BX, 0, 0);
 }
 
 void test_decode_mov_immediate_to_register_byte()
@@ -65,31 +62,44 @@ void test_decode_mov_immediate_to_register_wide()
 
 void test_decode_mov_memory_to_register_no_displacement()
 {
-	uint8_t mov_al_bx_plus_si[] = {0x8a, 0x00};
-	uint8_t mov_bx_bp_plus_di[] = {0x8b, 0x1b};
+	uint8_t mov_al_bx_si[] = {0x8a, 0x00};
+	uint8_t mov_bx_bp_di[] = {0x8b, 0x1b};
 	uint8_t mov_dx_bp[] = {0x8b, 0x56, 0x00};
 
 	Instruction instruction = {0};
 }
 
+void test_decode_mov_memory_to_register_direct()
+{
+	uint8_t mov_cx_30[] = {0x8b, 0x0e, 0x1e, 00};
+
+	Instruction instruction = decode_instruction(mov_cx_30);
+	TEST_INSTRUCTION(instruction, MOV_RM_REG, 4, CX, DIRECT, 30, 0);
+}
+
 void test_decode_mov_memory_to_register_byte_displacement()
 {
-	uint8_t mov_ah_bh_si_4[] = {0x8a, 0x60, 0x04};
+	uint8_t mov_ah_bx_si_4[] = {0x8a, 0x60, 0x04};
 
-	Instruction instruction = {0};
+	Instruction instruction = decode_instruction(mov_ah_bx_si_4);
+	TEST_INSTRUCTION(instruction, MOV_RM_REG, 3, AH, BX_SI_8, 4, 0);
 }
 
 void test_decode_mov_memory_to_register_wide_displacement()
 {
-	uint8_t mov_al_bx_si_5000[] = {0x8a, 0x80, 0x87, 0x13};
+	uint8_t mov_al_bx_si_5000[] = {0x8a, 0x80, 0x88, 0x13};
 
-	Instruction instruction = {0};
+	Instruction instruction = decode_instruction(mov_al_bx_si_5000);
+	TEST_INSTRUCTION(instruction, MOV_RM_REG, 4, AL, BX_SI_16, 5000, 0);
 }
 
 int main()
 {
 	UnityBegin("test.c");
 	RUN_TEST(test_decode_mov_reg_to_reg_byte);
-	RUN_TEST(test_decode_mov_immediate_to_register_byte);
+	RUN_TEST(test_decode_mov_reg_to_reg_wide);
+	RUN_TEST(test_decode_mov_memory_to_register_direct);
+	RUN_TEST(test_decode_mov_memory_to_register_byte_displacement);
+	RUN_TEST(test_decode_mov_memory_to_register_wide_displacement);
 	return UnityEnd();
 }
